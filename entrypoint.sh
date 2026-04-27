266f62817fbe322257a5ad00bdec69a087b8a1a6
Virrann <cunha.vargas.oliveira@gmail.com>
2026-04-27 08:04:57 -0300

    ajuste de compatibilidade ao vscode

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,10 +4,12 @@ echo "🚀 Starting Jupyter..."
 
 jupyter lab \
   --ip=0.0.0.0 \
-  --port="${JUPYTHER_PORT:-8888}"\
+  --port="${JUPYTHER_PORT:-8888}" \
   --no-browser \
   --allow-root \
-  --NotebookApp.token='' &
+  --ServerApp.token='' \
+  --ServerApp.password='' \
+  --ServerApp.disable_check_xsrf=True &
 
 echo "🖥️ Dropping into bash..."
 
